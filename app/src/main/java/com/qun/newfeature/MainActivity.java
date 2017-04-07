107035cb350913d2f84881c68f7ffeef0b9b9624
Qun <slvt@qq.com>
2017-04-07 22:54:44 +0800

    toolbar部分功能

--- app/src/main/java/com/qun/newfeature/MainActivity.java
+++ app/src/main/java/com/qun/newfeature/MainActivity.java
@@ -1,13 +1,32 @@
 package com.qun.newfeature;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToorbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToorbar = (Toolbar) findViewById(R.id.toolbar);
+        mToorbar.setTitle("主标题");
+        setSupportActionBar(mToorbar);
+        mToorbar.setSubtitle("子标题");
+        mToorbar.setTitleTextColor(Color.RED);
+        mToorbar.setNavigationIcon(R.mipmap.ic_launcher);
+        mToorbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "点击导航图标", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
