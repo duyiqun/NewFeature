7041e825c68d75de414d56404398000f807c263f
Qun <slvt@qq.com>
2017-04-09 00:38:25 +0800

    完善drawerlayout以及toolbar

--- app/src/main/java/com/qun/newfeature/MainActivity.java
+++ app/src/main/java/com/qun/newfeature/MainActivity.java
@@ -2,35 +2,52 @@ package com.qun.newfeature;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.menu.MenuBuilder;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToorbar;
+    private NavigationView mNavigationView;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //找到ToolBar控件
         mToorbar = (Toolbar) findViewById(R.id.toolbar);
-        mToorbar.setTitle("主标题");
-        setSupportActionBar(mToorbar);
+        mToorbar.setTitle("主标题");//如果想修改主标题，一定得在 setSupportActionBar(mToolbar);之前，否则没效果
+        //用这个ToolBar替换ActionBar
+        setSupportActionBar(mToorbar);//使用该代码之前必须确保：当前Activity使用的主题不能带有ActionBar
+        //给ToolBar添加子标题
         mToorbar.setSubtitle("子标题");
         mToorbar.setTitleTextColor(Color.RED);
-        mToorbar.setNavigationIcon(R.mipmap.ic_launcher);
-        mToorbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "点击导航图标", Toast.LENGTH_SHORT).show();
-            }
-        });
+//        mToorbar.setNavigationIcon(R.mipmap.ic_launcher);
+        //给ToolBar的导航图标添加事件
+//        mToorbar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "点击导航图标", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        //初始化NavigationView，跟ToolBar关联起来
+        mNavigationView = (NavigationView) findViewById(R.id.navigationView);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        //打开ToolBar的左侧的导航图标
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToorbar, R.string.open, R.string.close);
+        actionBarDrawerToggle.syncState();
+        mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+
     }
 
     @Override
@@ -48,15 +65,15 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.add:
 
-                Toast.makeText(this,"添加好友",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "添加好友", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.share:
 
-                Toast.makeText(this,"分享好友",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "分享好友", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.about:
 
-                Toast.makeText(this,"关于我们",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "关于我们", Toast.LENGTH_SHORT).show();
                 break;
         }
         return super.onOptionsItemSelected(item);
