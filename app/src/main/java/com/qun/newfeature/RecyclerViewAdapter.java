88fb580328179d99e5eacbd53bd44f09c03544ef
Qun <slvt@qq.com>
2017-04-10 17:39:39 +0800

    晚上adapter

--- app/src/main/java/com/qun/newfeature/RecyclerViewAdapter.java
+++ app/src/main/java/com/qun/newfeature/RecyclerViewAdapter.java
@@ -1,6 +1,7 @@
 package com.qun.newfeature;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -20,7 +21,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        //编写条目布局
+        //使用布局填充器将条目布局转换为View
+        /**
+         * 参数2：ViewGroup root ，如果传入null，那么在填充布局的时候会忽略布局上的宽和高属性
+         *  如果传入ViewGroup，那么在填充布局的时候就会考虑布局上的宽和高属性
+         * 参数3：是否让布局填充器将R.layout.list_item布局自动添加到ViewGroup上，如果传入true，那么返回值为parent本身而不再是R.layout.list_item转换的View了
+         */
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        //创建ViewHolder，并把View作为构造参数传递进去
+        RecyclerViewHolder recyclerViewHolder = new RecyclerViewHolder(view);
+        //将ViewHolder返回
+        return recyclerViewHolder;
     }
 
     @Override
